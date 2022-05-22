5c0653b9b1af1560c24640e698d6d9bcf04fe09a
Vova <uladzimirfilipau@gmail.com>
2022-05-22 13:40:36 +0300

    add Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../index.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__copyright">&copy; 2022. VOVA F.</p>
+    </footer>
+  );
+}
+
+export default Footer;
