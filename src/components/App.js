ae5038c77c6a3b265c552d9fbb52940cd0129531
Vova <uladzimirfilipau@gmail.com>
2022-05-24 15:38:41 +0300

    add userdata

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Main from "./Main";
 import PopupWithForm from "./PopupWithForm";
 import Footer from "./Footer";
+import api from "../utils/api.js";
+import handleError from "../utils/utils.js";
 
 function App() {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    api
+      .getProfileData()
+      .then((data) => {
+        setUser(data);
+      })
+      .catch(handleError);
+  }, []);
+
   return (
     <>
       <Header />
       <Main
+        userName={user.name}
+        userDescription={user.about}
+        userAvatar={user.avatar}
+        
         handleEditAvatarClick={() =>
           document
             .querySelector(".popup_edit-avatar")
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,6 +5,9 @@ function Main({
   handleEditAvatarClick,
   handleEditProfileClick,
   handleAddPlaceClick,
+  userName,
+  userDescription,
+  userAvatar,
 }) {
   return (
     <main className="content">
@@ -17,18 +20,18 @@ function Main({
             className="profile__avatar-edit"
             onClick={handleEditAvatarClick}
           />
-          <img src="#" alt="avatar" className="profile__avatar" />
+          <img src={userAvatar} alt="avatar" className="profile__avatar" />
         </div>
         <div className="profile__info">
           <div className="profile__title-container">
-            <h1 className="profile__title">VOVA F.</h1>
+            <h1 className="profile__title">{userName}</h1>
             <button
               aria-label="edit profile"
               className="profile__button-edit"
               onClick={handleEditProfileClick}
             ></button>
           </div>
-          <p className="profile__subtitle">Web-developer</p>
+          <p className="profile__subtitle">{userDescription}</p>
         </div>
         <button
           aria-label="add card"
