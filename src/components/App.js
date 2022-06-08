ba05cff63cb231837347353a4f928731341b5f46
Vova <uladzimirfilipau@gmail.com>
2022-06-08 11:00:57 +0300

    add catch after api

--- src/components/App.js
+++ src/components/App.js
@@ -49,15 +49,21 @@ function App() {
   function handleCardLike(card) {
     const isLiked = card.likes.some((i) => i._id === currentUser._id);
 
-    api.changeLikeCardStatus(card._id, !isLiked).then((newCard) => {
-      setCards((state) => state.map((c) => (c._id === card._id ? newCard : c)));
-    });
+    api
+      .changeLikeCardStatus(card._id, !isLiked)
+      .then((newCard) => {
+        setCards((state) => state.map((c) => (c._id === card._id ? newCard : c)));
+      })
+      .catch(handleError);
   }
 
   function handleCardDelete(card) {
-    api.deleteCard(card._id).then(() => {
-      setCards((cards) => cards.filter((c) => c._id !== card._id && c));
-    });
+    api
+      .deleteCard(card._id)
+      .then(() => {
+        setCards((cards) => cards.filter((c) => c._id !== card._id && c));
+      })
+      .catch(handleError);
   }
 
   function handleUpdateUser(data) {
